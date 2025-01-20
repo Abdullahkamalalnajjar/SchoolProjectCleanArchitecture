724bf1b4be31c126b8295d6910f8085912c15f6c
dell <abdallahalnajjar01@gmail.com>
2025-01-20 19:49:27 +0200

    last update

---
+++ SchoolProject.Data/Entities/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SchoolProject.Data.Entities.Identity;
+
+public class AppUser : IdentityUser
+{
+    public string? Address { get; set; }
+    public string? FullName { get; set; }
+    public string? Country { get; set; }
+    public string Password { get; set; }
+}
---
+++ SchoolProject.infrustructure/Migrations/20250120002031_adduseridentity3.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.infrustructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduseridentity3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
